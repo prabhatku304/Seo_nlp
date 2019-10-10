75f2bc57fd283069b037acf3fd9c5c0fa1600355
b117014 <b117014@iiit-bh.ac.in>
2019-10-10 22:37:09 +0530

    add frequency

--- tokenization.py
+++ tokenization.py
@@ -4,21 +4,28 @@ import nltk.corpus
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
+from collections import Counter
 
 corp='./data'
-filename = os.listdir(corp)
+root = os.listdir(corp)
+df=Counter()
+tfs={}
 
-file = open(os.path.join(corp, filename[0]), "r", encoding='UTF-8')
+for filename in root:
+    
+    file = open(os.path.join(corp, filename), "r", encoding='UTF-8')
+    
+    info = file.read()
+    file.close()
+    info = info.lower()
+    sw=set(stopwords.words('english'))
+    data_token = word_tokenize(info)
+    data_tokens = [token for token in data_token if token not in sw]
+    
+    tf=Counter(data_tokens)
+    df+=Counter(list(set(data_tokens)))
+    tfs[filename]=tf.copy()
+    tf.clear()
 
-info = file.read()
-file.close()
-info = info.lower()
-sw=set(stopwords.words('english'))
-data_token = word_tokenize(info)
-data_tokens = [token for token in data_token if token not in sw]
-
-fdist = FreqDist()
-for i in data_tokens:
-    fdist[i]+=1
-
-fdist
+df
+tfs
