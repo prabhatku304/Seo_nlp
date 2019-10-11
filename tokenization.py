b8f1998f1c65d69862f991013490fbe86a9d908f
b117014 <b117014@iiit-bh.ac.in>
2019-10-11 12:24:02 +0530

    query input

--- tokenization.py
+++ tokenization.py
@@ -10,7 +10,10 @@ corp='./data'
 root = os.listdir(corp)
 df=Counter()
 tfs={}
-
+vector={}
+posting_list = {}
+st_tokens = []
+lengths=Counter()
 for filename in root:
     
     file = open(os.path.join(corp, filename), "r", encoding='UTF-8')
@@ -27,5 +30,53 @@ for filename in root:
     tfs[filename]=tf.copy()
     tf.clear()
 
-df
-tfs
+
+
+for filename in tfs:
+    vector[filename]=Counter()
+    length=0
+    for token in tfs[filename]:
+        wt = weight(filename,token)
+        vector[filename][token]=wt
+        length+=wt**2
+        lengths[filename]=math.sqrt(length)
+        
+for filename in vector:
+    for token in vector[filename]:
+        vector[filename][token] = vector[filename][token] / lenghts[filename]
+        if token not in posting_list:
+            posting_list[token]=Counter()
+        posting_list[token][filename]=vector[filename][token]
+        
+def getWeight(filename,token):
+    return vector[filename][token]
+
+
+def query(inputstring):
+    inputstring = inputstring.lower()
+    qtf={}
+    qlength=0
+    ct=0
+    loc_docs={}
+    cos_sims=Counter()
+    q_tokens = word_tokenize(inputstring)
+    
+    for token in q_tokens:
+        token = stemmer.stem(token)
+        if token not in posting_list:
+            continue
+        if getidf(token)==0:
+            loc_docs[token],weight = zip(*posting_list[token].most_common())
+        else:
+            loc_docs[token],weight = zip(*posting_list[token].most_common(5))
+        
+        if ct==1:
+            track = set(loc_docs[token]) & track
+        else:
+            track = set(loc_docs[token])
+            ct=1
+        qtf[token] = 1+log10(inputstring.count(token))
+        qlength+=qtf[token]**2
+    qlength=sqrt(qlength)
+    for doc in vector:
+        
